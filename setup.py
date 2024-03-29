4f1fb8b349854d80b1c84040893a0dfe8b379e87
Vladimir Jovanovic <vladimirjo@gmail.com>
2024-03-29 21:19:20 +0100

    new commit

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from setuptools_scm.version import get_local_dirty_tag
-
-
-def clean_scheme(version):
-    return get_local_dirty_tag(version) if version.dirty else ""
-
-setup(use_scm_version={"local_scheme": clean_scheme})
